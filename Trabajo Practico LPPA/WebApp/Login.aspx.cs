d8a8dcc5cd2f351746aaf1a682b88de230597e99
Martin Dome <martindome96@gmail.com>
2022-11-06 21:36:49 -0300

    Cambio en los comentarios

--- Trabajo Practico LPPA/WebApp/Login.aspx.cs
+++ Trabajo Practico LPPA/WebApp/Login.aspx.cs
@@ -37,53 +37,6 @@ namespace WebApp
             }
             
         }
-        //protected void Button1_Click(object o, EventArgs e)
-        //{
-        //    usuarioBE = usuarioBLL.Verificar_Usuario_sinpassword(LoginControl.UserName.ToString());
-        //    //Verificar si existe y si esta bloqueado
-        //    if (!string.IsNullOrEmpty(usuarioBE.Usuario) && (usuarioBE.Bloqueado < 3))
-        //    {
-        //        //Verificar si la password esta correcta
-        //        usuarioBE = usuarioBLL.Verificar_Usuario(LoginControl.UserName.ToString(), LoginControl.Password.ToString());
-        //        if (!string.IsNullOrEmpty(usuarioBE.Usuario))
-        //        {
-        //            //Trae el perfil del usuario de la base (con composite)
-        //            usuarioBE.TipoUsuario.listaAcciones = usuarioBLL.Buscar_Acciones(usuarioBE.TipoUsuario.id);
-        //            //Bloqueado se pone 0
-        //            usuarioBLL.blanquear_password(LoginControl.UserName.ToString());
-        //            //Guardamos el objeto usuario en la variables de sesion
-        //            Session["usuario"] = usuarioBE;
-        //            //Llenamos la bitacora
-        //            Bitacora_BLL bitacoraBLL = new Bitacora_BLL();
-        //            string detalle = "Inicio de Sesion - Usuario: " + usuarioBE.Usuario;
-        //            bitacoraBLL.LLenar_Bitacora(usuarioBE.IdUsuario, detalle);
-        //            if (usuarioBE.TipoUsuario.id == 1)
-        //            {
-        //                Response.Redirect("Admin.aspx");
-        //            }
-        //            Response.Redirect("Default.aspx");
-        //        }
-        //        //contraseña incorrecta aumentar contador bloqueado en 1
-        //        else
-        //        {
-        //            usuarioBLL.Bloquear_usuario(LoginControl.UserName.ToString());
-        //            Label1.Text = "Usuario o clave invalida";
-        //            Label1.Visible = true;
-        //        }
-        //    }
-        //    else if (usuarioBE.Bloqueado == 3)
-        //    {
-        //        Label1.Text = "Su usuario esta bloqueado. Contactar administrador.";
-        //        Label1.Visible = true;
-        //    }
-
-        //    else
-        //    {
-        //        //Usuario o contraseña invalidos
-        //        Label1.Text = "Usuario o clave invalida";
-        //        Label1.Visible = true;
-        //    }
-        //}
 
         protected void Button2_Click(object o, EventArgs e)
         {
@@ -147,5 +100,53 @@ namespace WebApp
                 Label1.Visible = true;
             }
         }
+
+        //protected void Button1_Click(object o, EventArgs e)
+        //{
+        //    usuarioBE = usuarioBLL.Verificar_Usuario_sinpassword(LoginControl.UserName.ToString());
+        //    //Verificar si existe y si esta bloqueado
+        //    if (!string.IsNullOrEmpty(usuarioBE.Usuario) && (usuarioBE.Bloqueado < 3))
+        //    {
+        //        //Verificar si la password esta correcta
+        //        usuarioBE = usuarioBLL.Verificar_Usuario(LoginControl.UserName.ToString(), LoginControl.Password.ToString());
+        //        if (!string.IsNullOrEmpty(usuarioBE.Usuario))
+        //        {
+        //            //Trae el perfil del usuario de la base (con composite)
+        //            usuarioBE.TipoUsuario.listaAcciones = usuarioBLL.Buscar_Acciones(usuarioBE.TipoUsuario.id);
+        //            //Bloqueado se pone 0
+        //            usuarioBLL.blanquear_password(LoginControl.UserName.ToString());
+        //            //Guardamos el objeto usuario en la variables de sesion
+        //            Session["usuario"] = usuarioBE;
+        //            //Llenamos la bitacora
+        //            Bitacora_BLL bitacoraBLL = new Bitacora_BLL();
+        //            string detalle = "Inicio de Sesion - Usuario: " + usuarioBE.Usuario;
+        //            bitacoraBLL.LLenar_Bitacora(usuarioBE.IdUsuario, detalle);
+        //            if (usuarioBE.TipoUsuario.id == 1)
+        //            {
+        //                Response.Redirect("Admin.aspx");
+        //            }
+        //            Response.Redirect("Default.aspx");
+        //        }
+        //        //contraseña incorrecta aumentar contador bloqueado en 1
+        //        else
+        //        {
+        //            usuarioBLL.Bloquear_usuario(LoginControl.UserName.ToString());
+        //            Label1.Text = "Usuario o clave invalida";
+        //            Label1.Visible = true;
+        //        }
+        //    }
+        //    else if (usuarioBE.Bloqueado == 3)
+        //    {
+        //        Label1.Text = "Su usuario esta bloqueado. Contactar administrador.";
+        //        Label1.Visible = true;
+        //    }
+
+        //    else
+        //    {
+        //        //Usuario o contraseña invalidos
+        //        Label1.Text = "Usuario o clave invalida";
+        //        Label1.Visible = true;
+        //    }
+        //}
     }
 }
